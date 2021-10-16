a0c528afac54866d752466ee8206f1599c7155a9
Verdiktus <alex-0707@yandex.ru>
2021-10-16 14:24:04 +0300

    constructors

--- Base_Objects/Constructors/constructors.js
+++ Base_Objects/Constructors/constructors.js
@@ -5,3 +5,65 @@ function User(name) {
 
 let user = new User('Вася');
 console.log(user);
+
+
+let konichua = new User('Конишуа');
+konichua.isAdmin = true;
+console.log(konichua);
+
+function AnyUser(name) {
+  if(!new.target) {
+    return new AnyUser(name);
+  }
+  this.name = name;
+};
+
+let vasya = AnyUser('Василий');
+console.log(vasya);
+
+function HelloUser(name) {
+  this.name = name;
+
+  this.Hello = function() {
+    console.log('Меня зовут ' + this.name);
+  }
+  return this.Hello();
+}
+
+let hhuser = new HelloUser('Андрей');
+
+function Calculator() {
+  let x,y;
+  this.read = function() {
+    this.x = +prompt('Введите число x');
+    this.y = +prompt('Введите число y');
+    console.log(`Число x = ${this.x}, число y = ${this.y}`);
+    return this;
+  }
+  this.sum = function() {
+    console.log('Sum= ', this.x + this.y);
+    return this;
+  }
+
+  this.mul =function() {
+    console.log('Mul= ', this.x * this.y)
+    return this;
+  }
+}
+
+const calculator = new Calculator;
+//calculator.read().sum().mul();
+
+function Accumulator(startingValue) {
+    this.value = startingValue;  
+    
+    this.read = function() {
+    this.a = +prompt('Введите число для суммирования', 0);
+    this.value += this.a;
+    return this;
+  }
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read().read().read();
+console.log(accumulator.value);
