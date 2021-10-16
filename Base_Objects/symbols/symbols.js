c9f651b9e84e333ac13810a65bb5b3551dc71a1b
Verdiktus <alex-0707@yandex.ru>
2021-10-16 14:54:03 +0300

    add symbols

---
+++ Base_Objects/symbols/symbols.js
@@ -0,0 +1,13 @@
+let id = Symbol.for("id");
+let user = {
+  name: "Вася",
+  age: 30,
+  [id]: 123,
+};
+
+console.log(user);
+console.log('Напрямую '+user[id]);
+console.log(Object.keys(user));
+
+let clone = Object.assign({}, user);
+console.log(clone);
